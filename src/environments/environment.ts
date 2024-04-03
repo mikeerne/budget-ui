d1baac6d3a26892d43e061d2ca55621abbf17ca8
mikeerne <mike.erne@gmx.ch>
2024-04-03 17:27:15 +0200

    Login with OAuth2.0 configured and implemented

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,12 @@
 export const environment = {
   production: false,
   backendUrl: '/api',
+  firebaseConfig: {
+    apiKey: 'AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8',
+    authDomain: 'budget-planner-7eddb.firebaseapp.com',
+    projectId: 'budget-planner-7eddb',
+    appId: '1:278151755192:web:227ae9460314c348be4b4d',
+  },
 };
 
 /*
