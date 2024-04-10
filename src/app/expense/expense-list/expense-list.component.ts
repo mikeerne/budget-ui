838f509490f014c0c334879cc853dd0f8e3e442d
mikeerne <mike.erne@gmx.ch>
2024-04-10 22:15:51 +0200

    Ausgaben Anzeigen

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
 import { addMonths, set } from 'date-fns';
-import { ModalController } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, ModalController, RefresherCustomEvent } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
-import { Expense } from '../../shared/domain';
+import { Expense, ExpenseCriteria } from '../../shared/domain';
+import { formatPeriod } from '../../shared/period';
+import { finalize, from, groupBy, mergeMap, toArray } from 'rxjs';
+import { ExpenseService } from '../expense.service';
+import { ToastService } from '../../shared/service/toast.service';
 
+// interface for grouped expenses
+interface ExpenseGroup {
+  date: string;
+  expenses: Expense[];
+}
 @Component({
   selector: 'app-expense-overview',
   templateUrl: './expense-list.component.html',
 })
 export class ExpenseListComponent {
-  date = set(new Date(), { date: 1 });
-
-  constructor(private readonly modalCtrl: ModalController) {}
+  readonly initialSort = 'date,desc'; // add default sort for expenses, used for loadExpenses - sort by date
+  loading = false; // used for loadExpenses
+  date = set(new Date(), { date: 1 }); // used for loadExpenses
+  expenseGroups: ExpenseGroup[] | null = null; // used for loadExpenses
+  lastPageReached = false; // used for loadExpenses
+  searchCriteria: ExpenseCriteria = { page: 0, size: 50, sort: this.initialSort };
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly expenseService: ExpenseService,
+    private readonly toastService: ToastService,
+  ) {}
 
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
+    this.reloadExpenses(); // fix month selector
   };
 
   async openModal(expense?: Expense): Promise<void> {
@@ -24,6 +42,64 @@ export class ExpenseListComponent {
     });
     modal.present();
     const { role } = await modal.onWillDismiss();
+    if (role === 'refresh') this.loadExpenses(); // fix for hot-reload on add expenses
     console.log('role', role);
+  }
+
+  private loadExpenses(next: () => void = () => {}): void {
+    this.searchCriteria.yearMonth = formatPeriod(this.date);
+    if (!this.searchCriteria.categoryIds?.length) delete this.searchCriteria.categoryIds;
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    const groupByDate = this.searchCriteria.sort.startsWith('date');
+    this.expenseService
+      .getExpenses(this.searchCriteria)
+      .pipe(
+        finalize(() => (this.loading = false)),
+        mergeMap((expensePage) => {
+          this.lastPageReached = expensePage.last;
+          next();
+          if (this.searchCriteria.page === 0 || !this.expenseGroups) this.expenseGroups = [];
+          return from(expensePage.content).pipe(
+            groupBy((expense) => (groupByDate ? expense.date : expense.id)),
+            mergeMap((group) => group.pipe(toArray())),
+          );
+        }),
+      )
+      .subscribe({
+        next: (expenses: Expense[]) => {
+          const expenseGroup: ExpenseGroup = {
+            date: expenses[0].date,
+            expenses: this.sortExpenses(expenses),
+          };
+          const expenseGroupWithSameDate = this.expenseGroups!.find((other) => other.date === expenseGroup.date);
+          if (!expenseGroupWithSameDate || !groupByDate) this.expenseGroups!.push(expenseGroup);
+          else
+            expenseGroupWithSameDate.expenses = this.sortExpenses([
+              ...expenseGroupWithSameDate.expenses,
+              ...expenseGroup.expenses,
+            ]);
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not load expenses', error),
+      });
+  }
+
+  private sortExpenses = (expenses: Expense[]): Expense[] => expenses.sort((a, b) => a.name.localeCompare(b.name));
+
+  // refresher function
+  reloadExpenses($event?: any): void {
+    this.searchCriteria.page = 0;
+    this.loadExpenses(() => ($event ? ($event as RefresherCustomEvent).target.complete() : {}));
+  }
+
+  // infinite scroll function
+  loadNextExpensePage($event: any) {
+    this.searchCriteria.page++;
+    this.loadExpenses(() => ($event as InfiniteScrollCustomEvent).target.complete());
+  }
+
+  // load expenses on page load
+  ionViewDidEnter(): void {
+    this.loadExpenses();
   }
 }
